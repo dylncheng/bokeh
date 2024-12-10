99e729c16c9139a0ed30189b54831c2e6c81e53c
Dylan Cheng <chengdylan02@gmail.com>
2024-12-10 18:31:25 -0500

    added most recent changes

--- bokehjs/src/lib/models/formatters/image_tick_formatter.ts
+++ bokehjs/src/lib/models/formatters/image_tick_formatter.ts
@@ -3,31 +3,29 @@ import type {Arrayable} from "core/types"
 import {div} from "core/dom"
 
 export class ImageTickFormatter extends TickFormatter {
-//   static initClass(): void {
-//     this.prototype.type = "ImageTickFormatter"
-//   }
 
   image_source: {[key: string]: string | HTMLImageElement}
   source_type: string
 
   doFormat(ticks: Arrayable<number | string>, _opts: {loc: number}): string[] {
     const formatted: string[] = []
-    for (const tick of ticks) {
-      if (tick in this.image_source) {
+    for (const t of ticks) {
+      if (t in this.image_source) {
         if (this.source_type == "url") {
-          const img = div({style: {height: "20px"}}, div({style: {backgroundImage: `url(${this.image_source[tick]})`}}))
+          const img = div({style: {height: "10px"}}, div({style: {backgroundImage: `url(${this.image_source[t]})`}}))
           formatted.push(img.outerHTML)
+
         } else if (this.source_type == "array") {
-          const img = div({style: {height: "20px"}}, div({style: {backgroundImage: `url(data:image/png;base64,${this.image_source[tick]})`}}))
+          const img = div({style: {height: "10px"}}, div({style: {backgroundImage: `url(data:image/png;base64,${this.image_source[t]})`}}))
           formatted.push(img.outerHTML)
+
         } else {
           throw new Error(`Unsupported source type: ${this.source_type}`)
         }
       } else {
-        formatted.push(`${tick}`)  // Default to text if no image available
+        formatted.push(`${t}`) 
       }
     }
     return formatted
   }
 }
-// ImageTickFormatter.initClass()
